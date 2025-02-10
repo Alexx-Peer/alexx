8be226d534af4cefc073d48b8fdb2f643731ff61
Alexander <alexxpeer@icloud.com>
2025-02-09 20:09:07 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-console.log("Alexx Peer");
+const myName = 'Alexx Peer';
+let myDay = 'My Birthday Day';
+myDay = 'Sunday';
+const message = `Yo! My name is ${myName} and I won't tell you my birthday, but this year it's on ${myDay}!`;
+console.log(message);
